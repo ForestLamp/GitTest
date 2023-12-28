c3b0b5eb5c48e22e740876593ada32f916ddf130
Alex <y10b@ya.ru>
2023-12-28 19:35:26 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Alex on 28.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
